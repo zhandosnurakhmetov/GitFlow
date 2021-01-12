9be85fb7173f6fa704b91f0a449606073f897de1
zhandosnurakhmetov <zhnurakhmetov@one.kz>
2021-01-12 18:25:24 +0600

    initial commit

---
+++ Gitflow-sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gitflow-sample
+//
+//  Created by admin on 1/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
